da76ef82beb3b84521fcfd54edf86f0b90446ece
Ian Johnston <ianallanjohnston@gmail.com>
2022-09-12 17:31:48 -0700

    more analytics updates

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import ReactGA from "react-ga";
 const TRACKING_ID = "G-RZ3BKB4RVX";
 ReactGA.initialize(TRACKING_ID);
+ReactGA.pageview(window.location.pathname + window.location.search);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
