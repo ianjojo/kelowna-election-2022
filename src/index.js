7a5fa548d71f75148d6480cb34318848600c41e9
Ian Johnston <ianallanjohnston@gmail.com>
2022-09-12 17:29:00 -0700

    migrated to scss and also added google analytics code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
+import ReactGA from "react-ga";
+const TRACKING_ID = "G-RZ3BKB4RVX";
+ReactGA.initialize(TRACKING_ID);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
