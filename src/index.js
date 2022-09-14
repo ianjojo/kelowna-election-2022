c7e0492805a0a006511a7ce63c584d6382a5616e
Ian Johnston <ianallanjohnston@gmail.com>
2022-09-13 20:54:08 -0700

    updated react ga package to new 4

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 const TRACKING_ID = "G-RZ3BKB4RVX";
 ReactGA.initialize(TRACKING_ID);
 ReactGA.pageview(window.location.pathname + window.location.search);
